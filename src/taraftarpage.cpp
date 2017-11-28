c75a3f0e9e9fefbe78671d4d8691461677fb10b5
Cemal Demir <CMLDMR@users.noreply.github.com>
2017-11-28 13:13:25 +0300

    Taraftar Page Sayfası Oluşturuldu.

---
+++ src/taraftarpage.cpp
@@ -0,0 +1,6 @@
+#include "taraftarpage.h"
+
+TaraftarPage::TaraftarPage()
+{
+
+}
---
+++ src/taraftarpage.h
@@ -0,0 +1,12 @@
+#ifndef TARAFTARPAGE_H
+#define TARAFTARPAGE_H
+
+#include <QtCore/qglobal.h>
+
+class TaraftarPage : public WContainerWidget
+{
+public:
+    TaraftarPage();
+};
+
+#endif // TARAFTARPAGE_H
