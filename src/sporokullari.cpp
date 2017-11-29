4934354d20cc0b5541c7fac193364c4d4ec21d15
Cemal Demir <CMLDMR@users.noreply.github.com>
2017-11-29 17:48:47 +0300

    SporOkullari Eklendi

--- src/sporokullari.cpp
+++ src/sporokullari.cpp
@@ -1,6 +1,83 @@
 #include "sporokullari.h"
 
-SporOkullari::SporOkullari()
+Okullar::SporOkullari::SporOkullari()
 {
 
+}
+
+Okullar::SporOkullariWidget::SporOkullariWidget()
+{
+    addStyleClass(Bootstrap::Grid::container);
+    setContentAlignment(AlignmentFlag::Center);
+    auto mLayout = setLayout(cpp14::make_unique<WVBoxLayout>());
+    mLayout->setSpacing(0);
+    mLayout->setContentsMargins(0,0,0,0);
+
+
+    {
+        mLayout->addWidget(cpp14::make_unique<WText>("Spor Okulları"),0,AlignmentFlag::Center|AlignmentFlag::Middle)->addStyleClass("sporokullari");
+
+        auto container = mLayout->addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::row+Bootstrap::Test::border1px);
+        for( int i = 0 ; i < 8 ; i++ )
+        {
+            container->addWidget(cpp14::make_unique<item>())->addStyleClass(Bootstrap::Grid::Large::col_lg_3
+                                                                            +Bootstrap::Grid::Medium::col_md_3
+                                                                            +Bootstrap::Grid::Small::col_sm_6
+                                                                            +Bootstrap::Grid::ExtraSmall::col_xs_12+"okulitemBlock");
+        }
+    }
+
+}
+
+Okullar::SporOkullariWidget::item::item(std::string okuladi)
+{
+    addStyleClass(Bootstrap::Grid::container_fluid+Bootstrap::Test::border1px);
+
+    setMinimumSize(150,150);
+
+    auto mLayout = setLayout(cpp14::make_unique<WVBoxLayout>());
+
+    mLayout->addWidget(cpp14::make_unique<WText>(okuladi),0,AlignmentFlag::Center|AlignmentFlag::Middle);
+
+    decorationStyle().setBackgroundColor(WColor(qrand()%255,qrand()%255,qrand()%255));
+}
+
+Okullar::AktiviteWidget::AktiviteWidget()
+{
+
+    addStyleClass(Bootstrap::Grid::container);
+    setContentAlignment(AlignmentFlag::Center);
+    auto mLayout = setLayout(cpp14::make_unique<WVBoxLayout>());
+    mLayout->setSpacing(0);
+    mLayout->setContentsMargins(0,0,0,0);
+
+
+    {
+        mLayout->addWidget(cpp14::make_unique<WText>("Aktiviteler"),0,AlignmentFlag::Center|AlignmentFlag::Middle)->addStyleClass("sporokullari");
+
+        auto container = mLayout->addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::row+Bootstrap::Test::border1px);
+        for( int i = 0 ; i < 8 ; i++ )
+        {
+            container->addWidget(cpp14::make_unique<item>())->addStyleClass(Bootstrap::Grid::Large::col_lg_3
+                                                                            +Bootstrap::Grid::Medium::col_md_3
+                                                                            +Bootstrap::Grid::Small::col_sm_6
+                                                                            +Bootstrap::Grid::ExtraSmall::col_xs_12+"okulitemBlock");
+        }
+    }
+
+}
+
+Okullar::AktiviteWidget::item::item(std::string okuladi)
+{
+    addStyleClass(Bootstrap::Grid::container_fluid+Bootstrap::Test::border1px);
+
+    setMinimumSize(150,150);
+
+    auto mLayout = setLayout(cpp14::make_unique<WVBoxLayout>());
+
+    mLayout->addWidget(cpp14::make_unique<WText>(okuladi),0,AlignmentFlag::Center|AlignmentFlag::Middle);
+
+    decorationStyle().setBackgroundColor(WColor(qrand()%255,qrand()%255,qrand()%255));
 }
--- src/sporokullari.h
+++ src/sporokullari.h
@@ -3,10 +3,76 @@
 
 #include <QtCore/qglobal.h>
 
-class SporOkullari : public WContainerWidget
-{
-public:
-    SporOkullari();
-};
+#include <QString>
+#include <random>
+
+#include <Wt/WContainerWidget.h>
+#include <Wt/WText.h>
+#include <Wt/WHBoxLayout.h>
+#include <Wt/WVBoxLayout.h>
+#include <Wt/WImage.h>
+#include <Wt/WLink.h>
+#include <Wt/WCssDecorationStyle.h>
+#include <Wt/WMenuItem.h>
+#include <Wt/WTabWidget.h>
+#include <Wt/WTextArea.h>
+#include <Wt/WMenu.h>
+#include <Wt/WStackedWidget.h>
+#include <Wt/WText.h>
+#include <Wt/WTable.h>
+#include <Wt/WComboBox.h>
+
+
+#include <vector>
+
+#include "ToolKit/bootstrap.h"
+
+
+using namespace Wt;
+
+
+namespace Okullar {
+    class SporOkullari : public WContainerWidget
+    {
+    public:
+        SporOkullari();
+
+
+    };
+
+
+    class SporOkullariWidget : public WContainerWidget
+    {
+    public:
+        SporOkullariWidget();
+
+
+        class item : public WContainerWidget
+        {
+        public:
+            item(std::string okuladi = "Yaz Okulu Adı");
+        };
+    };
+
+
+
+    class AktiviteWidget : public WContainerWidget
+    {
+    public:
+        AktiviteWidget();
+
+        class item : public WContainerWidget
+        {
+        public:
+            item(std::string okuladi = "Aktivite Adı");
+        };
+    };
+
+
+
+
+}
+
+
 
 #endif // SPOROKULLARI_H
