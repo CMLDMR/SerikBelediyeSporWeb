a3596f4083717d445d5f0e34787f4d6ac6606f64
Cemal Demir <CMLDMR@users.noreply.github.com>
2017-11-30 10:46:51 +0300

    Basin Namespace Eklendi.

--- src/basinpage.h
+++ src/basinpage.h
@@ -3,10 +3,43 @@
 
 #include <QtCore/qglobal.h>
 
-class BasinPage : public WContainerWidget
-{
-public:
-    BasinPage();
-};
+#include <QString>
+#include <random>
+
+#include <Wt/WContainerWidget.h>
+#include <Wt/WText.h>
+#include <Wt/WHBoxLayout.h>
+#include <Wt/WVBoxLayout.h>
+#include <Wt/WImage.h>
+#include <Wt/WLink.h>
+#include <Wt/WCssDecorationStyle.h>
+#include <Wt/WMenuItem.h>
+#include <Wt/WTabWidget.h>
+#include <Wt/WTextArea.h>
+#include <Wt/WMenu.h>
+#include <Wt/WStackedWidget.h>
+#include <Wt/WText.h>
+#include <Wt/WTable.h>
+#include <Wt/WComboBox.h>
+
+
+#include <vector>
+
+#include "ToolKit/bootstrap.h"
+
+
+using namespace Wt;
+
+
+namespace Basin {
+    class BasinPage : public WContainerWidget
+    {
+    public:
+        BasinPage();
+    };
+}
+
+
+
 
 #endif // BASINPAGE_H
