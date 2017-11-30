fabe176ba4bc13d2f1b9ce7977aee24631b3b38c
Cemal Demir <CMLDMR@users.noreply.github.com>
2017-11-30 10:44:39 +0300

    Foto Widget ve Video Widget Eklendi.

--- src/fotovideopage.cpp
+++ src/fotovideopage.cpp
@@ -1,6 +1,96 @@
 #include "fotovideopage.h"
 
-FotoVideoPage::FotoVideoPage()
+FotoVideo::FotoVideoPage::FotoVideoPage()
 {
 
+}
+
+
+
+FotoVideo::FotoVideoWidget::FotoVideoWidget()
+{
+    addStyleClass(Bootstrap::Grid::container_fluid+Bootstrap::Test::border1px);
+
+    auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+    container->addStyleClass(Bootstrap::Grid::row);
+
+    container->addWidget(cpp14::make_unique<FotoWidget>())->addStyleClass(Bootstrap::Grid::Large::col_lg_6
+                                                                          +Bootstrap::Grid::Medium::col_md_6);
+    container->addWidget(cpp14::make_unique<VideoWidget>())->addStyleClass(Bootstrap::Grid::Large::col_lg_6
+                                                                           +Bootstrap::Grid::Medium::col_md_6);
+}
+
+
+
+FotoVideo::FotoVideoWidget::FotoWidget::FotoWidget()
+{
+    addStyleClass(Bootstrap::Grid::container_fluid+Bootstrap::Test::border1px);
+    setContentAlignment(AlignmentFlag::Center);
+
+
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::row);
+        container->addWidget(cpp14::make_unique<WText>("Foto Widget"))->addStyleClass(Bootstrap::Grid::Large::col_lg_12);
+    }
+
+
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::row);
+        for( int i = 0 ; i < 6 ; i++ )
+        {
+            container->addWidget(cpp14::make_unique<item>())->addStyleClass(Bootstrap::Grid::Large::col_lg_4
+                                                                            +Bootstrap::Grid::Medium::col_md_6
+                                                                            +Bootstrap::Grid::Small::col_sm_6
+                                                                            +Bootstrap::Grid::ExtraSmall::col_xs_12);;
+        }
+    }
+
+}
+
+FotoVideo::FotoVideoWidget::VideoWidget::VideoWidget()
+{
+    addStyleClass(Bootstrap::Grid::container_fluid+Bootstrap::Test::border1px);
+    setContentAlignment(AlignmentFlag::Center);
+
+
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::row);
+        container->addWidget(cpp14::make_unique<WText>("Video Widget"))->addStyleClass(Bootstrap::Grid::Large::col_lg_12);
+    }
+
+
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::row);
+        for( int i = 0 ; i < 6 ; i++ )
+        {
+            container->addWidget(cpp14::make_unique<item>())->addStyleClass(Bootstrap::Grid::Large::col_lg_4
+                                                                            +Bootstrap::Grid::Medium::col_md_6
+                                                                            +Bootstrap::Grid::Small::col_sm_6
+                                                                            +Bootstrap::Grid::ExtraSmall::col_xs_12);;
+        }
+    }
+}
+
+FotoVideo::FotoVideoWidget::FotoWidget::item::item()
+{
+    addStyleClass(Bootstrap::Test::border1px);
+    setMinimumSize(150,150);
+    decorationStyle().setBackgroundColor(WColor(qrand()%255,qrand()%255,qrand()%255));
+
+    auto mLayout = setLayout(cpp14::make_unique<WVBoxLayout>());
+    mLayout->addWidget(cpp14::make_unique<WText>("Foto item Widget"),0,AlignmentFlag::Center|AlignmentFlag::Middle);
+}
+
+FotoVideo::FotoVideoWidget::VideoWidget::item::item()
+{
+    addStyleClass(Bootstrap::Test::border1px);
+    setMinimumSize(150,150);
+    decorationStyle().setBackgroundColor(WColor(qrand()%255,qrand()%255,qrand()%255));
+
+    auto mLayout = setLayout(cpp14::make_unique<WVBoxLayout>());
+    mLayout->addWidget(cpp14::make_unique<WText>("Video item Widget"),0,AlignmentFlag::Center|AlignmentFlag::Middle);
 }
--- src/fotovideopage.h
+++ src/fotovideopage.h
@@ -3,10 +3,75 @@
 
 #include <QtCore/qglobal.h>
 
-class FotoVideoPage : public WContainerWidget
-{
-public:
-    FotoVideoPage();
-};
+#include <QString>
+#include <random>
+
+#include <Wt/WContainerWidget.h>
+#include <Wt/WText.h>
+#include <Wt/WHBoxLayout.h>
+#include <Wt/WVBoxLayout.h>
+#include <Wt/WImage.h>
+#include <Wt/WLink.h>
+#include <Wt/WCssDecorationStyle.h>
+#include <Wt/WMenuItem.h>
+#include <Wt/WTabWidget.h>
+#include <Wt/WTextArea.h>
+#include <Wt/WMenu.h>
+#include <Wt/WStackedWidget.h>
+#include <Wt/WText.h>
+#include <Wt/WTable.h>
+#include <Wt/WComboBox.h>
+
+
+#include <vector>
+
+#include "ToolKit/bootstrap.h"
+
+
+using namespace Wt;
+
+
+namespace FotoVideo {
+    class FotoVideoPage : public WContainerWidget
+    {
+    public:
+        FotoVideoPage();
+    };
+
+
+    class FotoVideoWidget : public WContainerWidget
+    {
+    public:
+        FotoVideoWidget();
+
+
+        class FotoWidget : public WContainerWidget
+        {
+        public:
+            FotoWidget();
+
+            class item : public WContainerWidget
+            {
+            public:
+                item();
+            };
+        };
+
+
+        class VideoWidget : public WContainerWidget
+        {
+        public:
+            VideoWidget();
+
+            class item : public WContainerWidget
+            {
+            public:
+                item();
+            };
+        };
+    };
+
+}
+
 
 #endif // FOTOVIDEOPAGE_H
