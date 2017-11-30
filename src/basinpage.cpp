179f25706f12fcf44bbe59ae598d47d880620362
Cemal Demir <CMLDMR@users.noreply.github.com>
2017-11-30 10:45:53 +0300

    Basın Class Eklendi.

---
+++ src/basinpage.cpp
@@ -0,0 +1,6 @@
+#include "basinpage.h"
+
+BasinPage::BasinPage()
+{
+
+}
---
+++ src/basinpage.h
@@ -0,0 +1,12 @@
+#ifndef BASINPAGE_H
+#define BASINPAGE_H
+
+#include <QtCore/qglobal.h>
+
+class BasinPage : public WContainerWidget
+{
+public:
+    BasinPage();
+};
+
+#endif // BASINPAGE_H
